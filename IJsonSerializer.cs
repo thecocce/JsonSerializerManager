6c4af8a66228209e352236f908597a3b1ca2050b
Jason Watson <jbw@jbw.cc>
2016-03-26 00:40:13 +0000

    JsonSerializer abstraction.

---
+++ IJsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JsonSerializer
+{
+    /// <summary>
+    /// Abstract serializer. 
+    /// Abstract away Json de/serializer implementation details. 
+    /// </summary>
+    public interface IJsonSerializer
+    {
+        byte[] SerializeToByteArray(object obj);
+        string SerializeToString<T>(T obj);
+        T Deserialize<T>(string jsonString);
+    }   
+}
---
+++ JsonSerializerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JsonSerializer
+{
+    /// <summary>
+    /// Simple wrapper for IJsonSerializer implementations. 
+    /// Additional domain specific functionality goes here. 
+    /// </summary>
+    public class JsonSerializerManager : IJsonSerializer
+    {
+        public JsonSerializerManager(IJsonSerializer serializer)
+        {
+            Serializer = serializer;
+        }
+
+        private readonly IJsonSerializer Serializer;
+
+        public byte[] SerializeToByteArray(object obj)
+        {
+            return Serializer.SerializeToByteArray(obj);
+        }
+
+        public string SerializeToString<T>(T obj)
+        {
+            return Serializer.SerializeToString<T>(obj);
+        }
+
+        public T Deserialize<T>(string jsonString)
+        {
+            return Serializer.Deserialize<T>(jsonString);
+        }
+    }
+}
